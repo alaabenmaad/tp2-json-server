82ed141ff5fd4d148f8b7709fde0d8b6f88bf9f4
alaa ben maad <alabenmaad1@gmail.com>
2022-11-17 12:33:20 +0100

    update table column

--- src/components/crud.vue
+++ src/components/crud.vue
@@ -10,13 +10,16 @@
                     <span v-if="showMsg" class="alert alert-success" role="alert"> {{msg}} </span>
                     <h1>
                         <br>
-                        Gestion des etudients
+                        Gestion Des Etudients
                         <br>
                     </h1>
                     <!-- Button trigger modal -->
                     <button type="button" class="btn btn-primary align-items-center pull-right m-2"
                         data-bs-toggle="modal" data-bs-target="#addmodal">
-                        Ajouter <i class="fa fa-plus fa-x"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                        </svg>
+                         Ajouter un etudiant 
                     </button>
 
                     <!-- AddModal -->
@@ -75,6 +78,8 @@
       <th scope="col">Email</th>
       <th scope="col">Classe</th>
       <th scope="col">Moyenne</th>
+      <th scope="col">Editer</th>
+      <th scope="col">Supprimer</th>
     </tr>
   </thead>
   <tbody>
